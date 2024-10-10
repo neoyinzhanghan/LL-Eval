64dde914c7a8dded5da35db6011371f86fb9e05d
Neo Yin <yinzhang@berkeley.edu>
2024-10-09 17:25:07 -0700

    max GPU memory used during inference

--- grab_differential_data.py
+++ grab_differential_data.py
@@ -7,8 +7,7 @@ from tqdm import tqdm
 from get_copath_data import get_diff, get_path_data
 from LLRunner.config import pipeline_run_history_path
 
-
-data_dir = "/media/hdd3/neo/results_dir"
+non_error_dir_csv_path = "/media/hdd3/neo/non_error_dirs.csv"
 
 
 def get_pipeline(pipeline_result_dir):
@@ -36,51 +35,30 @@ def get_slide_bar_code(pipeline_result_dir):
     return row["wsi_name"].values[0]
 
 
-# get the list of all subdirectories in the data directory
-all_subdirs = [
-    d for d in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, d))
-]
-
-# onlu keep the one that starts with BMA and PBS
-result_dirs = [d for d in all_subdirs if "BMA-diff" in d or "PBS-diff" in d]
-
-all_result_dir_paths = [os.path.join(data_dir, d) for d in result_dirs]
-
-
-num_errors = 0
-num_dirs = len(all_result_dir_paths)
-non_error_dirs = []
-
-all_cell_paths = []
+# turn the result_dir_path column into a list
+non_error_dirs_df = pd.read_csv(non_error_dir_csv_path)
+non_error_dirs = non_error_dirs_df["result_dir_path"].tolist()
 
 diff_rows = []
 
 i = 0
-for result_dir_path in tqdm(all_result_dir_paths, desc="Filtering out error dirs:"):
+for result_dir_path in tqdm(non_error_dirs, desc="Filtering out error dirs:"):
     while i < 10:
-        # check if the result_dir_path contains a file called "error.txt"
-        if not os.path.exists(os.path.join(result_dir_path, "error.txt")):
-            non_error_dirs.append(result_dir_path)
 
-            wsi_name = get_slide_bar_code(result_dir_path)
-            pipeline = get_pipeline(result_dir_path)
+        wsi_name = get_slide_bar_code(result_dir_path)
+        pipeline = get_pipeline(result_dir_path)
 
-            accession_number = wsi_name.split(";")[0]
+        accession_number = wsi_name.split(";")[0]
 
-            if pipeline == "BMA-diff":
-                new_path_df_row = get_path_data(accession_number)
-                new_df_row = get_diff(new_path_df_row)
+        if pipeline == "BMA-diff":
+            new_path_df_row = get_path_data(accession_number)
+            new_df_row = get_diff(new_path_df_row)
 
-                i += 1
+            i += 1
 
-            diff_rows.append(new_df_row)
+        diff_rows.append(new_df_row)
 
-        else:
-            num_errors += 1
 
 diff_df = pd.DataFrame(diff_rows)
 
 diff_df.to_csv("/media/hdd3/neo/test_diff_df.csv", index=False)
-
-print(f"Number of error dirs: {num_errors}")
-print(f"Number of non-error dirs: {len(non_error_dirs)}")
