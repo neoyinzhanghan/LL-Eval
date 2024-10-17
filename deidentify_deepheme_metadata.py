ebac956fb7a4b2756aa6a8f0751a594ff4ecf45b
Neo Yin <yinzhang@berkeley.edu>
2024-10-17 09:59:15 -0700

    script for getting differential for MDS from copath database

--- deidentify_deepheme_metadata.py
+++ deidentify_deepheme_metadata.py
@@ -41,4 +41,4 @@ for idx, row in tqdm(cell_metadata.iterrows(), desc="Deidentifying cell metadata
 deiden_df = pd.DataFrame(deidentified_cell_df_dict)
 
 # save the deidentified cell metadata to a csv file
-deiden_df.to_csv("deidentified_cell_metadata_normal.csv", index=False)
+deiden_df.to_csv("deidentified_cell_metadata.csv", index=False)
